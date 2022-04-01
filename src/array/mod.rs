1252b5fca09f9cc3ff6a6834b80f88d1b9c77e8c
Hao Xiang <haoxiangzhao@outlook.com>
2022-04-01 09:34:49 +0800

    差分数组

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -2081,7 +2081,7 @@ pub mod windows {
 
 /// # 前缀和/前缀树
 ///
-/// 特点: 前缀; 区间;
+/// 特点: 前缀; 区间; 原数组不动
 ///
 /// ## 题目
 /// * 简单
@@ -2092,10 +2092,13 @@ pub mod windows {
 /// * 困难
 pub mod pre_sum {
 
+
+    #[allow(dead_code)]
     struct NumArray {
         pre: Vec<i32>,
     }
 
+    #[allow(dead_code)]
     impl NumArray {
         fn new(nums: Vec<i32>) -> Self {
             let mut pre = vec![0];
@@ -2123,10 +2126,13 @@ pub mod pre_sum {
         }
     }
 
+
+    #[allow(dead_code)]
     struct NumMatrix {
         pre: Vec<Vec<i32>>,
     }
 
+    #[allow(dead_code)]
     impl NumMatrix {
         fn new(matrix: Vec<Vec<i32>>) -> Self {
             let (row, col) = (matrix.len(), matrix.first().unwrap().len());
@@ -2155,14 +2161,14 @@ pub mod pre_sum {
     }
 
     /// [560. 和为 K 的子数组](https://leetcode-cn.com/problems/subarray-sum-equals-k/)
-    /// 
+    ///
     /// 和[1. 两数之和](https://leetcode-cn.com/problems/two-sum/) 相似, 只是这个是两数之差
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
         use std::collections::HashMap;
 
         let mut curr = 0;
         let mut store = HashMap::new();
-        
+
         // 用来处理 前缀和 恰好等于k 的情况
         // store.insert(0, 1);
 
@@ -2170,10 +2176,10 @@ pub mod pre_sum {
         for num in nums {
             curr += num;
             // 或者手动判是否为k, 手动+1
-            if curr == k{
+            if curr == k {
                 ret += 1;
             }
-            
+
             // 两数之差, 目标值
             let target = curr - k;
             ret += store.get(&target).unwrap_or(&0);
@@ -2188,10 +2194,10 @@ pub mod pre_sum {
         use std::collections::HashMap;
 
         let mut counter: HashMap<i32, Vec<usize>> = HashMap::new();
-        for (idx, &num) in nums.iter().enumerate(){
+        for (idx, &num) in nums.iter().enumerate() {
             let other = target - num;
-            if let Some(v) = counter.get(&other){
-                return vec![v[0] as i32 , idx as i32];
+            if let Some(v) = counter.get(&other) {
+                return vec![v[0] as i32, idx as i32];
             }
             let entry = counter.entry(num).or_insert(Vec::<usize>::new());
             entry.push(idx);
@@ -2232,12 +2238,12 @@ pub mod pre_sum {
                     k: 0,
                     expect: 0,
                 },
-                TestCase{
+                TestCase {
                     name: "fix 2",
                     nums: &[-1, -1, 1],
                     k: 0,
-                    expect: 1
-                }
+                    expect: 1,
+                },
             ]
             .iter()
             .for_each(|testcase| {
@@ -2348,6 +2354,196 @@ pub mod pre_sum {
                         assert_eq!(*expect, actual, "{} {} failed", testcase.name, &idx);
                     });
             })
+        }
+    }
+}
+
+/// # 差分数组
+///
+/// 特点:
+/// * 区间
+/// * 与前缀比: 原数组变动
+///
+/// 步骤:
+/// 1. 差分: `diff[0] = origin[0]; diff[i] = origin[i] - origin[i-1]`
+/// 2. 操作: `diff[i] += inc; diff[j] -= inc`
+/// 3. 还原: `origin[0] = diff[0]; origin[i] = origin[i-1] + diff[i]`
+///
+/// ## 题目
+/// * 简单
+/// * 中等
+///     * [370. 区间加法](https://leetcode-cn.com/problems/range-addition/)
+///     *[1109. 航班预订统计](https://leetcode-cn.com/problems/corporate-flight-bookings/)
+/// * 困难
+pub mod diff_sub {
+
+    /// [370. 区间加法](https://leetcode-cn.com/problems/range-addition/)
+    ///
+    pub fn get_modified_array(length: i32, updates: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut ret = vec![0; length as usize];
+        let mut diff = vec![0; length as usize];
+
+        for update in updates.iter() {
+            let (start, end, inc) = (update[0] as usize, update[1] as usize, update[2]);
+            diff[start] += inc;
+            if end + 1 < diff.len() {
+                diff[end + 1] -= inc;
+            }
+        }
+
+        // restore
+        ret[0] = diff[0];
+        for i in 1..ret.len() {
+            ret[i] = diff[i] + ret[i - 1];
+        }
+        ret
+    }
+
+    /// [1109. 航班预订统计](https://leetcode-cn.com/problems/corporate-flight-bookings/)
+    pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
+        let mut ret = vec![0; n as usize];
+        let mut diff = vec![0; n as usize];
+
+        for booking in bookings.iter() {
+            let (start, end, inc) = (booking[0] as usize - 1, booking[1] as usize - 1, booking[2]);
+            diff[start] += inc;
+            if end + 1 < diff.len() {
+                diff[end + 1] -= inc;
+            }
+        }
+
+        // restore
+        ret[0] = diff[0];
+        for i in 1..ret.len() {
+            ret[i] = diff[i] + ret[i - 1];
+        }
+        ret
+    }
+
+    /// [1094. 拼车](https://leetcode-cn.com/problems/car-pooling/)
+    pub fn car_pooling(trips: Vec<Vec<i32>>, capacity: i32) -> bool {
+        let end = { trips.iter().map(|trip| trip[2]).max().unwrap() };
+
+        let mut counter = vec![0; end as usize];
+        let mut diff = vec![0; end as usize];
+
+        for trip in trips.iter() {
+            // [from, to) 到了就下车, 不包含在内
+            let (cnt, from, to) = (trip[0], trip[1] as usize, trip[2] as usize - 1);
+
+            diff[from] += cnt;
+            if to + 1 < diff.len() {
+                diff[to + 1] -= cnt;
+            }
+        }
+        // restore
+        counter[0] = diff[0];
+        for i in 1..counter.len() {
+            counter[i] = diff[i] + counter[i - 1];
+        }
+
+        counter.iter().all(|cnt| *cnt <= capacity)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_car_pooling() {
+            struct TestCase {
+                name: &'static str,
+                trips: &'static [&'static [i32]],
+                capacity: i32,
+                expect: bool,
+            }
+
+            vec![
+                TestCase {
+                    name: "basic",
+                    trips: &[&[2, 1, 5], &[3, 3, 7]],
+                    capacity: 4,
+                    expect: false,
+                },
+                TestCase {
+                    name: "basic 2",
+                    trips: &[&[2, 1, 5], &[3, 3, 7]],
+                    capacity: 5,
+                    expect: true,
+                },
+                TestCase {
+                    name: "fix 1",
+                    trips: &[&[2, 1, 5], &[3, 5, 7]],
+                    capacity: 3,
+                    expect: true,
+                },
+                TestCase {
+                    name: "fix 2",
+                    trips: &[&[9, 0, 1], &[3, 3, 7]],
+                    capacity: 4,
+                    expect: false,
+                },
+            ]
+            .iter()
+            .for_each(|testcase| {
+                let trips = testcase.trips.iter().map(|x| x.to_vec()).collect();
+                let actual = car_pooling(trips, testcase.capacity);
+                assert_eq!(testcase.expect, actual, "{} failed", testcase.name);
+            });
+        }
+
+        #[test]
+        fn test_corp_flight_bookings() {
+            struct TestCase {
+                name: &'static str,
+                bookings: &'static [&'static [i32]],
+                n: i32,
+                expect: &'static [i32],
+            }
+
+            vec![
+                TestCase {
+                    name: "basic",
+                    bookings: &[&[1, 2, 10], &[2, 3, 20], &[2, 5, 25]],
+                    n: 5,
+                    expect: &[10, 55, 45, 25, 25],
+                },
+                TestCase {
+                    name: "basic 2",
+                    bookings: &[&[1, 2, 10], &[2, 2, 15]],
+                    n: 2,
+                    expect: &[10, 25],
+                },
+            ]
+            .iter()
+            .for_each(|testcase| {
+                let bookings = testcase.bookings.iter().map(|x| x.to_vec()).collect();
+                let actual = corp_flight_bookings(bookings, testcase.n);
+                assert_eq!(testcase.expect, actual, "{} failed", testcase.name);
+            });
+        }
+
+        #[test]
+        fn test_get_modified_array() {
+            struct TestCase {
+                name: &'static str,
+                length: i32,
+                updates: &'static [&'static [i32]],
+                expect: &'static [i32],
+            }
+
+            vec![TestCase {
+                name: "basic",
+                length: 5,
+                updates: &[&[1, 3, 2], &[2, 4, 3], &[0, 2, -2]],
+                expect: &[-2, 0, 3, 5, 3],
+            }]
+            .iter()
+            .for_each(|testcase| {
+                let updates = testcase.updates.iter().map(|x| x.to_vec()).collect();
+                let actual = get_modified_array(testcase.length, updates);
+                assert_eq!(testcase.expect, actual, "{} failed", testcase.name);
+            });
         }
     }
 }
