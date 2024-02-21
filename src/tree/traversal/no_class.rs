07ba8c0e889f3006f03d4610cd5c99dc7f2328d5
Hao Xiang <haoxiangzhao@outlook.com>
2024-02-21 09:50:34 +0800

    tree travel

--- src/tree/traversal/no_class.rs
+++ src/tree/traversal/no_class.rs
@@ -1,3 +1,5 @@
+//! 多种遍历方式混合的
+
 use datastructure::TreeNode;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -18,17 +20,44 @@ pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<Tr
         let root_val = preorder[0];
         let mut root = TreeNode::new(root_val);
 
+        // 题目保证值不同, 因此可以直接遍历寻找
         let root_idx = inorder.iter().position(|&x| x == root_val).unwrap();
         let (inorder_left, inorder_right) = inorder.split_at(root_idx);
 
-        let inorder_left_len = inorder_left.len();
-        let (preorder_left, preorder_right) = preorder.split_at(inorder_left_len + 1);
-
-        root.left = build(&preorder_left[1..], inorder_left);
-        root.right = build(preorder_right, &inorder_right[1..]);
+        root.left = build(&preorder[1..1+inorder_left.len()], inorder_left);
+        root.right = build(&preorder[1+inorder_left.len()..], &inorder_right[1..]);
         Some(Rc::new(RefCell::new(root)))
     }
     build(&preorder, &inorder)
+}
+
+/// [106. 从中序与后序遍历序列构造二叉树](https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)
+///
+/// ## 思路
+/// 1. 与[105. 从前序与中序遍历序列构造二叉树](build_tree)类似, 通过中序做分割, 然后递归左右子树
+/// 2. 区别是后序遍历的根节点在最后
+pub fn build_tree2(inorder: Vec<i32>, postorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    fn build(inorder: &[i32], postorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if postorder.is_empty() {
+            return None;
+        }
+
+        let root_val = postorder.last().unwrap();
+        let mut root = TreeNode::new(*root_val);
+
+        // 题目保证值不同, 因此可以直接遍历寻找
+        let root_idx = inorder.iter().position(|&x| x == *root_val).unwrap();
+        let (inorder_left, inorder_right) = inorder.split_at(root_idx);
+
+        root.left = build(inorder_left, &postorder[..inorder_left.len()]);
+        root.right = build(
+            &inorder_right[1..],
+            &postorder[inorder_left.len()..postorder.len() - 1],
+        );
+        Some(Rc::new(RefCell::new(root)))
+    }
+
+    build(&inorder, &postorder)
 }
 
 #[cfg(test)]
@@ -42,6 +71,17 @@ mod tests {
         let preorder = vec![3, 9, 20, 15, 7];
         let inorder = vec![9, 3, 15, 20, 7];
         let root = build_tree(preorder, inorder);
+        assert_eq!(
+            root,
+            tree!({3, left: {9}, right:{20, left: {15}, right:{7} }})
+        );
+    }
+
+    #[test]
+    fn test_build_tree2() {
+        let inorder = vec![9, 3, 15, 20, 7];
+        let postorder = vec![9, 15, 7, 20, 3];
+        let root = build_tree2(inorder, postorder);
         assert_eq!(
             root,
             tree!({3, left: {9}, right:{20, left: {15}, right:{7} }})
